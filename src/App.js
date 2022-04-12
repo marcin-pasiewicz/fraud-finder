d3eb65b1a89c18f87a10eaf9f1989537f8adb49e
Marcin Pasiewicz <marcin.pasiewicz@acoustic.co>
2022-04-12 16:49:16 +0200

    feat: add benford score

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import { Field, Form, Formik } from 'formik';
+import { useState } from 'react';
+import { BenfordsLaw } from 'benfordslaw';
 import './App.css';
 
 function App() {
+  const [benfordScore, setBenfordScore] = useState();
+  const encoder = new TextEncoder()
+
+  function handleSubmit(values) {
+    const { story } = values;
+    const textArray = [...encoder.encode(story)];
+    const benfords = new BenfordsLaw(textArray);
+    const probability = benfords.getProbability()
+    setBenfordScore(probability);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Formik
+        initialValues={{
+          story: '',
+        }}
+        onSubmit={handleSubmit}
+      >
+        <Form>
+          <label htmlFor="story" className='story-label'>Tell me something</label>
+          <Field id="story" name="story" placeholder="Enter your text here" as="textarea" className='story-text' />
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
+      <div>score: {benfordScore}</div>
+      <div>probably: {benfordScore >= 0.9 ? 'fiction' : 'true story'}</div>
     </div>
   );
 }
